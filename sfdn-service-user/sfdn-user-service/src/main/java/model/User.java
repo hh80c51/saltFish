c046a7583a862d59b7c9f3de572270307ef1a0c9
hanghe <hanghe@hongkunjinfu.com>
2020-01-13 18:11:26 +0800

    bootstrap模板

---
+++ sfdn-service-user/sfdn-user-service/src/main/java/model/User.java
@@ -0,0 +1,46 @@
+package model;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String tel;
+
+    public User(Integer id, String name, String tel) {
+        this.id = id;
+        this.name = name;
+        this.tel = tel;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tel='" + tel + '\'' +
+                '}';
+    }
+}
