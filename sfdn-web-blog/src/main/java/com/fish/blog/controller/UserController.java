bbf49f6cc32ea88f1b9a9dbebc9f500e21d147a3
hehang <hanghe@hongkunjinfu.com>
2020-05-21 01:16:13 +0800

    服务提供者注册到zookeeper

--- sfdn-web-blog/src/main/java/com/fish/blog/controller/UserController.java
+++ sfdn-web-blog/src/main/java/com/fish/blog/controller/UserController.java
@@ -3,12 +3,15 @@ package com.fish.blog.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.fish.user.model.User;
 import com.fish.user.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Reference
     private UserService userService;
@@ -16,8 +19,8 @@ public class UserController {
     @RequestMapping("userInfo")
     public String findUserInfo(){
         System.out.println("方法：findUserInfo");
-//        User user = userService.getUser(1);
-//        System.out.println(user.getName());
+        User user = userService.getUser(1);
+        System.out.println(user.getName());
         return "/user";
     }
 }
