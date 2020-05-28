9cc47eae2c75bc438db3784197bbfbdd0d3864d7
hehang <hanghe@hongkunjinfu.com>
2020-05-28 22:52:47 +0800

    首页商品样式调整

--- sfdn-web-blog/src/main/web/page/shop/controllerJs/index.js
+++ sfdn-web-blog/src/main/web/page/shop/controllerJs/index.js
@@ -16,20 +16,20 @@ $(function () {
                         productArea += '<div class="product">';
                         productArea += '<div class="product_image"><img src="${project_base_path}'+ product.cover +'" alt=""></div>';
                             if(i == productList.length - 1){
-                                productArea += '<div class="product_extra product_new"><a href="categories.html">New</a></div>"';
+                                productArea += '<div class="product_extra product_new"><a href="categories.html">New</a></div>';
                             }
                             if(i == productList.length - 2){
                                 productArea += '<div class="product_extra product_sale"><a href="categories.html">Sale</a></div>';
                             }
-                        productArea += '     <div class="product_content">'
-                        productArea += '         <div class="product_title"><a href="product.html">'+ product.name +'</a></div>"'
-                        productArea += '         <div class="product_price">$'+ product.price +'</div>'
-                        productArea += '     </div>'
+                        productArea += '<div class="product_content">';
+                        productArea += '<div class="product_title"><a href="product.html">'+ product.name +'</a></div>';
+                        productArea += '<div class="product_price">$'+ product.price +'</div>';
                         productArea += '</div>';
+                        productArea += '</div>';;
                     }
                 }
                 $(".product_grid").empty();
-                $(".product_grid").html(productArea);
+                $(".product_grid").append(productArea);
             } else {
                 //无商品信息
             }
