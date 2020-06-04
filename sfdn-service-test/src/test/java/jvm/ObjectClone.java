8a3da6834170c68dbb4f16e966797c53b8123ae3
hehang <hanghe@hongkunjinfu.com>
2020-06-05 00:25:57 +0800

    浅拷贝和深拷贝

---
+++ sfdn-service-test/src/test/java/jvm/ObjectClone.java
@@ -0,0 +1,11 @@
+package jvm;
+
+import com.fish.user.model.User;
+
+public class ObjectClone {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setId(1);
+        user.setName("hh");
+    }
+}
