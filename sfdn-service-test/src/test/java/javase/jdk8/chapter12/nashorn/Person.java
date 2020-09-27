c5ddde0f24540bd401a682b280f19266a579d082
hh80c51 <hehanger@163.com>
2020-09-27 18:29:35 +0800

    java8函数式编程案例

---
+++ sfdn-service-test/src/test/java/javase/jdk8/chapter12/nashorn/Person.java
@@ -0,0 +1,16 @@
+package javase.jdk8.chapter12.nashorn;
+
+/**
+ * Created by benhail on 2014/7/13.
+ */
+public class Person {
+    String firstName;
+    String lastName;
+
+    Person() {}
+
+    Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
